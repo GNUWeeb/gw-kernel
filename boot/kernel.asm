079addc890e4cdf51ab6bea4ce455098d84de034
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2021-08-15 14:29:12 +0700

    boot: fix .gitignore, add linker script and kernel entry point
    boot/.gitignore:
    We have already had the rule to ignore all *.bin files in the main
    .gitignore. So this rule is unnecessary in the sub .gitignore, remove
    it!
    
    The rest is about linker script and kernel entry point.
    
    Extra: I created make.sh for helper to make it easy to build without
    doing export everytime we leave the terminal.
    
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

---
+++ boot/kernel.asm
@@ -0,0 +1,37 @@
+;
+; SPDX-License-Identifier: GPL-2.0
+;
+; Copyright (C) 2021  Ammar Faizi <ammarfaizi2@gmail.com>
+;
+
+[BITS 32]
+global _start
+
+CODE_SEG equ 0x08
+DATA_SEG equ 0x10
+
+_start:
+.end:
+	mov	ax, DATA_SEG
+	mov	ds, ax
+	mov	es, ax
+	mov	fs, ax
+	mov	gs, ax
+	mov	ss, ax
+	mov	ebp, 0x00200000
+	mov	esp, ebp
+	cli
+
+	;
+	; Enable the A20 line
+	; Ref: https://wiki.osdev.org/A20_Line
+	;
+	in	al, 0x92
+	or	al, 0x2
+	out	0x92, al
+
+.end_loop:
+	hlt
+	jmp	.end_loop
+
+	times 512 - ($ - $$) db 0
