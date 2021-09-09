aa933306c6a76814e15a46760c2555d008f4c7b6
Ammar Faizi <ammarfaizi2@gmail.com>
2021-09-09 17:03:31 +0700

    lib/string: add memmove as memcpy alias
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- include/gwk/string.h
+++ include/gwk/string.h
@@ -3,8 +3,8 @@
  * Copyright (C) 2021  Ammar Faizi <ammarfaizi2@gmail.com>
  */
 
-#ifndef STRING_H
-#define STRING_H
+#ifndef GWK__STRING_H
+#define GWK__STRING_H
 
 #include <stddef.h>
 #include <stdint.h>
@@ -12,5 +12,6 @@
 extern void *memset(void *p, int c, size_t len);
 extern void *memcpy(void *__restrict__ dst, const void *__restrict__ src,
 		    size_t len);
+extern void *memmove(void *dst, const void *src, size_t len);
 
-#endif /* #ifndef STRING_H */
+#endif /* #ifndef GWK__STRING_H */
