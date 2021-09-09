17183b9a337864833278c83547c95a4eabb88e06
Ammar Faizi <ammarfaizi2@gmail.com>
2021-09-09 21:58:05 +0700

    boot/kernel_entry.asm: remap the master PIC
    Remap the PIC in order to get IRQ number starts from the given number.
    
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- boot/kernel_entry.asm
+++ boot/kernel_entry.asm
@@ -27,6 +27,17 @@ _start:
 	or	al, 0x2
 	out	0x92, al
 
+	; Remap the master PIC.
+	mov	al, 0b00010001
+	out	0x20, al	; Tell the master PIC.
+
+	mov	al, 0x20	; Interrupt 0x20 is where the master ISR should start.
+	out	0x21, al
+
+	mov	al, 0b00000001
+	out	0x21, al
+	; End of remap the master PIC.
+
 	xor	ebp, ebp
 	mov	esp,  0x200000
 	call	kernel_main
